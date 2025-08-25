c5bbe60256f22f6be663e8eed686a398440aecc3
epicharshgupta <harshitguptapranjul@gmail.com>
2025-08-25 22:52:23 +0530

    change url

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -9,7 +9,7 @@ import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 const App = () => {
-  const url="http://localhost:4000"
+  const url="https://food-delivery-app-tz9j.onrender.com"
 
   return (
     <div>
